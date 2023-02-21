26c7d5663de9231989a32e517bda68999a5d6ee1
Dypnotic <jenscherukad@gmail.com>
2023-02-21 14:45:07 +0100

    New remaps

--- lua/dypnotic/remap.lua
+++ lua/dypnotic/remap.lua
@@ -2,5 +2,9 @@ local map = vim.api.nvim_set_keymap
 
 map('n', '<leader>n', '<cmd>bnext<CR>', {noremap = true})
 map('n', '<leader>p', '<cmd>bprev<CR>', {noremap = true})
+map('n', '<leader>b', '<cmd>buffers<CR>', {noremap = true})
+map('n', '<leader>t', '<cmd>tabnew<CR>', {noremap = true})
+map('n', '<leader>T', '<cmd>tabclose<CR>', {noremap = true})
+map('n', '<leader>s', '<cmd>tabsplit<CR>', {noremap = true})
 map('n', '<leader>q', '<cmd>q<CR>', {noremap = true})
 map('n', '<leader>w', '<cmd>w<CR>', {noremap = true})
