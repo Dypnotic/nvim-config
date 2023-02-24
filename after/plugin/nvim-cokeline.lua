b2d556c0a63005e62bd51109688e23502537c2e1
Dypnotic <jenscherukad@gmail.com>
2023-02-24 16:23:45 +0100

    Add modified icon for cokeline, format mapping

--- after/plugin/nvim-cokeline.lua
+++ after/plugin/nvim-cokeline.lua
@@ -1,1 +1,38 @@
-require('cokeline').setup()
+local get_hex = require('cokeline/utils').get_hex
+require('cokeline').setup {
+	components = {
+		{
+			text = function(buffer)
+				return " " .. buffer.devicon.icon
+			end,
+			fg = function(buffer)
+				return buffer.devicon.color
+			end,
+		},
+		{
+			text = function(buffer)
+				return buffer.unique_prefix
+			end,
+			fg = function(buffer)
+				return buffer.is_focused and "#B24BF3" or "#cdcdcd"
+			end,
+			style = "italic",
+		},
+		{
+			text = function(buffer)
+				return buffer.filename
+			end,
+			style = function(buffer)
+				return buffer.is_focused and "bold" or nil
+			end,
+		},
+		{
+			text = function(buffer)
+				return buffer.is_modified and " ● " or " "
+			end,
+			fg = function(buffer)
+				return buffer.is_focused and get_hex('Normal', 'bg') or get_hex('Normal', 'fg')
+			end,
+		},
+	}
+}
