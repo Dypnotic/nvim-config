608dac6109016e3b7ad80e4590549a51e953972e
Dypnotic <jenscherukad@gmail.com>
2023-05-23 12:53:28 +0200

    telescope-projects setup

--- lua/dypnotic/packer.lua
+++ lua/dypnotic/packer.lua
@@ -16,20 +16,20 @@ return require('packer').startup(function(use)
 		branch = 'v1.x',
 		requires = {
 			-- LSP Support
-			{ 'neovim/nvim-lspconfig' }, -- Required
-			{ 'williamboman/mason.nvim' }, -- Optional
+			{ 'neovim/nvim-lspconfig' },          -- Required
+			{ 'williamboman/mason.nvim' },        -- Optional
 			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
 			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' }, -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
-			{ 'hrsh7th/cmp-buffer' }, -- Optional
-			{ 'hrsh7th/cmp-path' }, -- Optional
+			{ 'hrsh7th/nvim-cmp' },      -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' },  -- Required
+			{ 'hrsh7th/cmp-buffer' },    -- Optional
+			{ 'hrsh7th/cmp-path' },      -- Optional
 			{ 'saadparwaiz1/cmp_luasnip' }, -- Optional
-			{ 'hrsh7th/cmp-nvim-lua' }, -- Optional
+			{ 'hrsh7th/cmp-nvim-lua' },  -- Optional
 
 			-- Snippets
-			{ 'L3MON4D3/LuaSnip' }, -- Required
+			{ 'L3MON4D3/LuaSnip' },          -- Required
 			{ 'rafamadriz/friendly-snippets' }, -- Optional
 		}
 	}
@@ -37,19 +37,11 @@ return require('packer').startup(function(use)
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		-- or                            , branch = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' } },
-	}
-
-	-- Telescope file-browser
-	use {
-    "nvim-telescope/telescope-file-browser.nvim",
-    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-	}
-
-	-- Telescope Projects
-	use {
-		"nvim-telescope/telescope-project.nvim",
-		requires = { "nvim-telescope/telescope.nvim" }
+		requires = {
+			{ 'nvim-lua/plenary.nvim' },
+			{ 'nvim-telescope/telescope-project.nvim' }, -- Telescope Projects
+			{ 'nvim-telescope/telescope-file-browser.nvim' } -- Telescope File Browser
+		},
 	}
 
 	-- Treesitter
