7c2374dd819f751312075dd197079456720bfa1b
Dypnotic <jenscherukad@gmail.com>
2023-11-07 21:35:50 +0100

    update lsp-zero

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,68 +1,96 @@
 return {
 	{
 		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
+		branch = 'v3.x',
+		lazy = true,
+		config = false,
+		init = function()
+			-- Disable automatic setup, we are doing it manually
+			vim.g.lsp_zero_extend_cmp = 0
+			vim.g.lsp_zero_extend_lspconfig = 0
+		end,
+	},
+	{
+		'williamboman/mason.nvim',
+		lazy = false,
+		config = true,
+	},
+
+	-- Autocompletion
+	{
+		'hrsh7th/nvim-cmp',
+		event = 'InsertEnter',
 		dependencies = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },          -- Required
-			{ 'williamboman/mason.nvim' },        -- Optional
-			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },      -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' },  -- Required
-			{ 'hrsh7th/cmp-buffer' },    -- Optional
-			{ 'hrsh7th/cmp-path' },      -- Optional
-			{ 'saadparwaiz1/cmp_luasnip' }, -- Optional
-			{ 'hrsh7th/cmp-nvim-lua' },  -- Optional
-
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },          -- Required
-			{ 'rafamadriz/friendly-snippets' }, -- Optional
+			{ 'L3MON4D3/LuaSnip' },
 		},
 		config = function()
-			local lsp = require('lsp-zero').preset({
-				name = 'minimal',
-				set_lsp_keymaps = true,
-				manage_nvim_cmp = true,
-				suggest_lsp_servers = false,
+			-- Here is where you configure the autocompletion settings.
+			local lsp_zero = require('lsp-zero')
+			lsp_zero.extend_cmp()
+			lsp_zero.set_sign_icons({
+				error = '🤬',
+				warn = '🔔',
+				hint = '💚',
+				info = '💦'
 			})
+			-- And you can configure cmp even more, if you want to.
+			local cmp = require('cmp')
+			local cmp_action = lsp_zero.cmp_action()
 
-			lsp.ensure_installed({
-				'tsserver',
-				'eslint',
-				'lua_ls',
-				'cssls',
-				'html',
-			})
-			-- (Optional) Configure lua language server for neovim
-			lsp.nvim_workspace()
-
-			lsp.on_attach(function(client, bufnr)
-				local opts = { buffer = bufnr, remap = false }
-
-				vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-				vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-				vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-				vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-				vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-				vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-				vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-				vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-				vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-				vim.keymap.set("n", "<leader>fm", function() vim.lsp.buf.format() end, opts)
-				vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-			end)
-
-			lsp.setup()
-
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = true,
-				float = {
-					source = 'always',
-				}
+			cmp.setup({
+				formatting = lsp_zero.cmp_format(),
+				mapping = cmp.mapping.preset.insert({
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<CR>'] = cmp.mapping.confirm({ select = false }),
+					['<Tab>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+					['<S-Tab>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+					['<C-f>'] = cmp_action.luasnip_jump_forward(),
+					['<C-b>'] = cmp_action.luasnip_jump_backward(),
+				})
 			})
 		end
 	},
+
+	-- LSP
+	{
+		'neovim/nvim-lspconfig',
+		cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+		event = { 'BufReadPre', 'BufNewFile' },
+		dependencies = {
+			{ 'hrsh7th/cmp-nvim-lsp' },
+			{ 'williamboman/mason-lspconfig.nvim' },
+		},
+		config = function()
+			-- This is where all the LSP shenanigans will live
+			local lsp_zero = require('lsp-zero')
+			lsp_zero.extend_lspconfig()
+
+			lsp_zero.on_attach(function(_, bufnr)
+				-- see :help lsp-zero-keybindings
+				-- to learn the available actions
+				lsp_zero.default_keymaps({ buffer = bufnr, remap = false })
+				local opts = { buffer = bufnr, remap = false }
+				vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+				vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+				vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+			end)
+
+			require('mason-lspconfig').setup({
+				ensure_installed = {
+					'tsserver',
+					'cssls',
+					'html',
+					'lua_ls'
+				},
+				handlers = {
+					lsp_zero.default_setup,
+					lua_ls = function()
+						-- (Optional) Configure lua language server for neovim
+						local lua_opts = lsp_zero.nvim_lua_ls()
+						require('lspconfig').lua_ls.setup(lua_opts)
+					end,
+				}
+			})
+		end
+	}
 }
