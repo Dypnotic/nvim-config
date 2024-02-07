52f6b8d915d2617ef300ac322283ba9510ffebe8
Dypnotic <jenscherukad@gmail.com>
2024-02-07 13:39:53 +0100

    Add nvim-colorizer

---
+++ lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,10 @@
+return {
+	'NvChad/nvim-colorizer.lua',
+	opts = {
+		filetypes = { 'css', 'scss', 'sass', 'less', 'javascript', 'typescript', 'html' },
+		user_default_options = {
+			css = true,
+			css_fn = true
+		}
+	}
+}
