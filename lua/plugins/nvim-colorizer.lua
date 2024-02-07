460398542d298b8f1b0c4f2c4e11cf5525b3a21a
Dypnotic <jenscherukad@gmail.com>
2024-02-07 14:47:42 +0100

    Add jsx and tsx to colorize

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,7 +1,7 @@
 return {
 	'NvChad/nvim-colorizer.lua',
 	opts = {
-		filetypes = { 'css', 'scss', 'sass', 'less', 'javascript', 'typescript', 'html' },
+		filetypes = { 'css', 'scss', 'sass', 'less', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'html' },
 		user_default_options = {
 			css = true,
 			css_fn = true
