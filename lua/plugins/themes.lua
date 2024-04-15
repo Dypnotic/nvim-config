ddd0320aca32843b4bd9c34f73315d87ba44ad14
Dypnotic <jenscherukad@gmail.com>
2024-04-15 16:10:13 +0200

    update theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,14 +1,17 @@
 return {
 	{
 		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
 		opts = {
 			style = 'storm',
 		},
-		config = function ()
-			vim.cmd([[colorscheme tokyonight]])
-		end
 	},
-	{ "rose-pine/neovim", name = "rose-pine" }
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		config = function ()
+			vim.cmd([[colorscheme rose-pine-dawn]])
+		end
+	}
 }
