b145b5c86830e10e7787fec0b1f820ce87df9e36
Dypnotic <jenscherukad@gmail.com>
2024-09-16 21:12:10 +0200

    Update transparency

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -3,6 +3,11 @@ return {
 		"folke/tokyonight.nvim",
 		opts = {
 			style = 'storm',
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
 		},
 	},
 	{
@@ -10,7 +15,21 @@ return {
 		name = "rose-pine",
 		lazy = false,
 		priority = 1000,
-		config = function ()
+		config = function()
+			require('rose-pine').setup {
+				styles = {
+					transparency = true
+				},
+				highlight_groups = {
+					TelescopeBorder = { fg = "highlight_high", bg = "none" },
+					TelescopeNormal = { bg = "none" },
+					TelescopePromptNormal = { bg = "base" },
+					TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+					TelescopeSelection = { fg = "text", bg = "base" },
+					TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+					CursorLine = { bg = 'none'},
+				}
+			}
 			vim.cmd([[colorscheme rose-pine-main]])
 		end
 	}
