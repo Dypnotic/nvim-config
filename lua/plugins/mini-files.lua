aa5194fe898c86229a5224c6f73f06770fd46402
Dypnotic <jenscherukad@gmail.com>
2024-12-11 11:32:56 +0100

    Switch from oil to mini.files (even better)

---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,8 @@
+return {
+	'echasnovski/mini.files',
+	version = false,
+	config = function()
+		require('mini.files').setup()
+		vim.keymap.set("n", "<leader>e", '<cmd>lua MiniFiles.open()<CR>')
+	end
+}
