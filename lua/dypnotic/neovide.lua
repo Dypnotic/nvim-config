091e8e2e23a6e128b8b9c5efb51e6190c10a7397
Dypnotic <jenscherukad@gmail.com>
2024-12-12 01:04:54 +0100

    Update neovide fonts

--- lua/dypnotic/neovide.lua
+++ lua/dypnotic/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-	vim.o.guifont = "JetBrainsMono NF:h9";
+	vim.o.guifont = "JetBrainsMono Nerd Font:h9";
 	vim.g.neovide_refresh_rate = 120;
 	vim.g.neovide_remember_window_size = true;
 	vim.g.neovide_cursor_animation_length = 0.03;
