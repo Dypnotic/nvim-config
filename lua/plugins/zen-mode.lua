0bdb0dd7579d3a2035f27fb039fe1579ae0ff958
Dypnotic <jenscherukad@gmail.com>
2025-08-25 12:10:28 +0200

    Enable statusbar in zen mode

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -12,6 +12,18 @@ return {
 			backdrop = 1,
 
 		},
+		plugins = {
+			-- disable some global vim options (vim.o...)
+			-- comment the lines to not apply the options
+			options = {
+				enabled = true,
+				ruler = true, -- disables the ruler text in the cmd line area
+				showcmd = true, -- disables the command in the last line of the screen
+				-- you may turn on/off statusline in zen mode by setting 'laststatus'
+				-- statusline will be shown only if 'laststatus' == 3
+				laststatus = 3, -- turn off the statusline in zen mode
+			},
+		},
 		neovide = {
 			enabled = true,
 			-- Will multiply the current scale factor by this number
