9f35bf570b0512c42987f85d51451fa2804d93ed
Dypnotic <jenscherukad@gmail.com>
2025-09-18 15:22:53 +0200

    Chore: update mini.files

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -1,8 +1,10 @@
 return {
-	'echasnovski/mini.files',
+	'nvim-mini/mini.files',
 	version = false,
+	keys = {
+		{'<leader>e', '<cmd>lua MiniFiles.open()<CR>'}, desc = 'Open file explorer',
+	},
 	config = function()
 		require('mini.files').setup()
-		vim.keymap.set("n", "<leader>e", '<cmd>lua MiniFiles.open()<CR>')
-	end
+	end,
 }
